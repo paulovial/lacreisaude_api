6050fe628f7ff913e058242645a4de1d1dde803b
user01 <paulovitormvd@gmail.com>
2025-04-05 20:22:40 -0300

    pre-docker

--- lacreisaude-backend/app/consultas/models.py
+++ lacreisaude-backend/app/consultas/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Profissional(models.Model):
+    nome_social = models.CharField(max_length=255)
+    profissao = models.CharField(max_length=255)
+    endereco = models.CharField(max_length=255)
+    contato = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.nome_social} ({self.profissao})"
+
+
+class Consulta(models.Model):
+    data = models.DateTimeField()
+    profissional = models.ForeignKey(Profissional, on_delete=models.CASCADE, related_name="consultas")
+
+    def __str__(self):
+        return f"Consulta com {self.profissional.nome_social} em {self.data}"
+

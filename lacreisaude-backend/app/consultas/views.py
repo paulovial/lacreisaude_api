66edc42f43443d792786bb0dc5e184d972d62194
user01 <paulovitormvd@gmail.com>
2025-04-07 19:54:24 -0300

    Asaas

--- lacreisaude-backend/app/consultas/views.py
+++ lacreisaude-backend/app/consultas/views.py
@@ -9,3 +9,46 @@ class ConsultaViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['profissional']
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Consulta
+
+@api_view(['POST'])
+def mock_pagamento(request):
+    consulta_id = request.data.get('consulta_id')
+    valor_total = request.data.get('valor_total')
+
+    if not consulta_id or not valor_total:
+        return Response(
+            {"erro": "consulta_id e valor_total são obrigatórios."},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    try:
+        consulta = Consulta.objects.get(id=consulta_id)
+    except Consulta.DoesNotExist:
+        return Response({"erro": "Consulta não encontrada."}, status=404)
+
+    # Simula o split
+    valor_total = float(valor_total)
+    valor_profissional = round(valor_total * 0.8, 2)
+    valor_lacrei = round(valor_total * 0.2, 2)
+
+    return Response({
+        "status_pagamento": "RECEBIDO",
+        "consulta_id": consulta.id,
+        "valor_total": valor_total,
+        "split_pagamento": {
+            "profissional": {
+                "id": consulta.profissional.id,
+                "nome": consulta.profissional.nome_social,
+                "valor_recebido": valor_profissional
+            },
+            "plataforma": {
+                "nome": "Lacrei Saúde",
+                "valor_recebido": valor_lacrei
+            }
+        }
+    }, status=status.HTTP_200_OK)
+

521d7ae9c64a0e42da0b193f96ad13cb358b6ebc
user01 <paulovitormvd@gmail.com>
2025-04-07 20:33:29 -0300

    asaas 2

--- lacreisaude-backend/app/consultas/urls.py
+++ lacreisaude-backend/app/consultas/urls.py
@@ -1,8 +1,12 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ConsultaViewSet
+from .views import ConsultaViewSet, mock_pagamento
 
 router = DefaultRouter()
 router.register(r'consultas', ConsultaViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+    path('pagamentos/mock/', mock_pagamento, name='mock_pagamento'),
+]
 
